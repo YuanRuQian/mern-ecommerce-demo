dfaab545d47b05777e14c515e74cb17151781d3c
Lydia Yuan <lydiayuan99@gmail.com>
2024-05-31 15:38:05 -0400

    add: prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,27 @@
-import { useEffect, useState } from 'react'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import ProductList, { Product } from './componenets/ProductList'
-import Header from './componenets/Header'
-
+import { useEffect, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ProductList, { Product } from "./componenets/ProductList";
+import Header from "./componenets/Header";
 
 const theme = createTheme({
   // other theme properties
 });
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
-
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch(`http://localhost:5050/product`)
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  })
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  });
 
   return (
     <ThemeProvider theme={theme}>
-    <Header />
-    <ProductList products={products} />
-</ThemeProvider>
-  )
+      <Header />
+      <ProductList products={products} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import RootLayout from './componenets/RootLayout.tsx';
-import Register from './componenets/Register.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./componenets/RootLayout.tsx";
+import Register from "./componenets/Register.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
-      <Register />,
+    element: <Register />,
     errorElement: <div>404 Not Found</div>,
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RootLayout>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </RootLayout>
   </React.StrictMode>,
-)
+);
